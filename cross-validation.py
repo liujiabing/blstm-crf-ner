931fa09370cd5b965e0d5c9b624eff1aa680b3a9
emrekgn <emrekaganakkaya@gmail.com>
2018-04-05 10:59:59 +0300

    get real-time output from subprocess

--- cross-validation.py
+++ cross-validation.py
@@ -84,10 +84,15 @@ for train_index, test_index in rs.split(sentences):
     # Build
     with open('output.log', 'a+') as out:
         out.write("Beginning building for CV iteration:{}".format(str(count)))
-        p = subprocess.Popen('python3 build_data.py', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        retval = p.wait()
-        p_out = p.stdout.read().decode('utf-8')
-        out.write(p_out + '\n')
+        p = subprocess.Popen('python3 build_data.py', shell=True, stdout=subprocess.PIPE, universal_newlines = True, stderr=subprocess.STDOUT)
+        while True:
+            output = p.stdout.readline()
+            if output == '' and p.poll() is not None:
+                break
+            if output:
+                out.write(output.strip() + '\n')
+                out.flush()
+        retval = p.poll()
         out.write("Finished building. exit code:{}\n".format(str(retval)))
         out.flush()
     print("Built model.")
@@ -95,10 +100,15 @@ for train_index, test_index in rs.split(sentences):
     # Train
     with open('output.log', 'a+') as out:
         out.write("Beginning training for CV iteration:{}".format(str(count)))
-        p = subprocess.Popen('python3 train.py', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        retval = p.wait()
-        p_out = p.stdout.read().decode('utf-8')
-        out.write(p_out + '\n')
+        p = subprocess.Popen('python3 train.py', shell=True, stdout=subprocess.PIPE, universal_newlines = True, stderr=subprocess.STDOUT)
+        while True:
+            output = p.stdout.readline()
+            if output == '' and p.poll() is not None:
+                break
+            if output:
+                out.write(output.strip() + '\n')
+                out.flush()
+        retval = p.poll()
         out.write("Finished training. exit code:{}\n".format(str(retval)))
         out.flush()
     print("Trained model.")
@@ -106,10 +116,15 @@ for train_index, test_index in rs.split(sentences):
     # Evaluate
     with open('output.log', 'a+') as out:
         out.write("Beginning eval for CV iteration:{}".format(str(count)))
-        p = subprocess.Popen('python3 evaluate.py', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        retval = p.wait()
-        p_out = p.stdout.read().decode('utf-8')
-        out.write(p_out + '\n')
+        p = subprocess.Popen('python3 evaluate.py', shell=True, stdout=subprocess.PIPE, universal_newlines = True, stderr=subprocess.STDOUT)
+        while True:
+            output = p.stdout.readline()
+            if output == '' and p.poll() is not None:
+                break
+            if output:
+                out.write(output.strip() + '\n')
+                out.flush()
+        retval = p.poll()
         out.write("Finished eval. exit code:{}\n".format(str(retval)))
         out.flush()
     print("Evaluated model.")
